6b987a03c84bcbc33198bea1b54f27b291496e2a
aroos <aroosnasir@gmail.com>
2026-05-16 17:24:52 +0500

    practice things
    practice things

---
+++ practiceStringOperations.py
@@ -0,0 +1,16 @@
+#my practice code
+LahoreInfo = "Lahore is the capital of Punjab and Islamabad is capital of pakistan"
+print(LahoreInfo)
+print(type(LahoreInfo))
+
+print(LahoreInfo.lower())
+print(LahoreInfo.upper())
+print(LahoreInfo.title())
+print(LahoreInfo.swapcase())
+print(LahoreInfo.capitalize())
+print(LahoreInfo.count("capital"))
+
+print(LahoreInfo[0:6:1])
+print(LahoreInfo[14:22:1])
+
+print(len(LahoreInfo))
