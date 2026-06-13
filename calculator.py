80037af7002a99418d3d89cb96b0f0730e633743
aroos <aroosnasir@gmail.com>
2026-06-13 19:25:54 +0500

    commit 3
    commit 3

---
+++ calculator.py
@@ -0,0 +1,8 @@
+
+
+x = float(input ("what is twelve plus twelve?"))
+y = float(input ("what is three times thirteen?"))
+
+z = x * y
+
+print(f"{z}")
--- week4/realstate-data.py
+++ week4/realstate-data.py
@@ -8,3 +8,15 @@ print(price)
 print(city)
 print(prev_sold_date)
 
+print ('RealEstate-USA.csv mean:' , np.mean(price))
+print ('RealEstate-USA.csv median:' , np.median(price))
+print ('RealEstate-USA.csv standard deviation:' , np.std(price))
+print ('RealEstate-USA.csv variance' , np.var(price))
+print ("RealEstate-USA.csv percentile - 25:", np.percentile(price, 25))
+print ('RealEstate-USA.csv percentile - 3') , np.percentile(price,3)
+print ('RealEstate-USA.csv min:', np.min(price))
+print ('RealEstate-USA.csv max:' , np.max(price))
+
+
+#math operations
+
